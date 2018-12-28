65e7b2cf85547ef2b40c49c616e361a5ba9ed7e0
rsaless <rspavarina@gmail.com>
2018-12-27 22:28:03 -0200

    Componente de notificações
    criação do componente de notificações

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
+import { AlertModule } from '../shared/components/alert/alert.module';
 
 @NgModule({
     declarations: [HeaderComponent, FooterComponent],
     exports: [HeaderComponent, FooterComponent],
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, AlertModule],
     providers: [{
         provide: HTTP_INTERCEPTORS,
         useClass: RequestInterceptor,
--- alurapic/src/app/photos/photo-detail/photo-detail.component.ts
+++ alurapic/src/app/photos/photo-detail/photo-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 import { Observable } from 'rxjs';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
     templateUrl: './photo-detail.component.html'
@@ -15,17 +17,32 @@ export class PhotoDetailComponent implements OnInit{
     constructor(
         private route: ActivatedRoute, 
         private photoService: PhotoService,
-        private router: Router
+        private router: Router,
+        private alertService: AlertService,
+        private userService: UserService
     ){}
 
     ngOnInit(): void {
         this.photoId = this.route.snapshot.params.photoId;
         this.photo$ = this.photoService.findById(this.photoId);
+        this.photo$.subscribe(()=>{}, err => {
+            console.log(err);
+            this.router.navigate(['not-found']);
+        })
     }
 
     remove(){
         this.photoService
             .removePhoto(this.photoId)
-            .subscribe(() => this.router.navigate(['']));
+            .subscribe(
+                () => {
+                    this.alertService.success("Photo removed", true);
+                    this.router.navigate(['/user', this.userService.getUserName() ]);
+                },
+                err => {
+                    console.log(err);
+                    this.alertService.warning("Could not delete the photo!");
+                }
+            );
     }
 }
